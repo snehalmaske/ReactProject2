18ebe6a4f06701c1ed2c448d7fe01673628399c2
snehalmaske <snehalmaske@users.noreply.github.com>
2019-08-02 12:07:00 -0700

    Face Recognition App
    This App allows the user to detect face in the image provided.

---
+++ facerecognition/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+         {/*<FaceRecognition />*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ facerecognition/src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ImageLinkForm = () => {
+	return(
+		<div>
+		<p className= 'f3'>
+		{'This Magic brain will detect faces in your pictures. Enjoy :)'}
+		</p>
+		<div>
+			
+
+		</div>
+		</div>
+
+		);
+}
+
+export default ImageLinkForm;
