6ac9a251ada0a42aac9853c0610a56c169c76b3f
snehalmaske <snehalmaske@users.noreply.github.com>
2019-08-05 11:16:28 -0700

    Adding styling to the App

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
-function App() {
+
+const particlesOptions ={
+	particles: {
+        number : {
+        	value: 50,
+        	density:{
+        		enable:true,
+        		value_area: 800
+        	}
+
+        }
+            		
+    }
+}
+
+class App extends Component {
+	render(){
   return (
     <div className="App">
+    <Particles className= 'particles'
+              params={particlesOptions}
+              
+            />
+
       <Navigation />
       <Logo />
       <ImageLinkForm />
          {/*<FaceRecognition />*/}
     </div>
   );
+	};
 }
 
 export default App;
