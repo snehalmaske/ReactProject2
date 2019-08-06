22fff18c6d517966c309f24965597ad66873511f
snehalmaske <snehalmaske@users.noreply.github.com>
2019-08-06 13:20:46 -0700

    Image display
    Image is displayed after the detect button is clicked.

--- facerecognition/src/App.js
+++ facerecognition/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: 'feb5d30072e941d19b8ef2a992929948'
+});
 
 
 const particlesOptions ={
@@ -21,18 +27,47 @@ const particlesOptions ={
 }
 
 class App extends Component {
-	render(){
+	constructor() {
+		super();
+		this.state = {
+			input: '' ,
+			imageUrl: ''
+		}
+	}
+
+	onInputChange = (event) => {
+		this.setState({input: event.target.value});
+	}
+
+	onButtonSubmit = () => {
+		this.setState({imageUrl: this.state.input});
+		app.models.predict(
+			Clarifai.FACE_DETECT_MODEL, 
+			this.state.input)
+		.then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
+}
+
+render(){
   return (
     <div className="App">
     <Particles className= 'particles'
               params={particlesOptions}
               
-            />
+    />
 
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-         {/*<FaceRecognition />*/}
+    <Navigation />
+    <Logo />
+    <ImageLinkForm 
+      onInputChange = {this.onInputChange} 
+      onButtonSubmit = {this.onButtonSubmit}/>
+    <FaceRecognition imageUrl={this.state.imageUrl} />
     </div>
   );
 	};
