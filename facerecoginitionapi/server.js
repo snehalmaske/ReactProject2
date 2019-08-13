84dcbab756004bdf913eeae712d9feda909fe7a5
snehalmaske <snehalmaske@users.noreply.github.com>
2019-08-13 13:05:30 -0700

    Connecting the front-end with the server

--- facerecoginitionapi/server.js
+++ facerecoginitionapi/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const database = {
 	users: [
@@ -31,7 +33,7 @@ const database = {
 }
 
 app.get('/', (req, res)=> {
-	res.send('this is working');
+	res.send(database.users);
 })
 
 app.post('/signin', (req, res) =>{
@@ -41,6 +43,49 @@ app.post('/signin', (req, res) =>{
 	} else {
 		res.status(400).json('error logging in');
 	}
+})
+
+app.post('/register', (req, res) => {
+	const{ email, password, name } = req.body;
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		entries: 0,
+		joined: new Date()
+
+	})
+	res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found= false;
+	database.users.forEach(user =>{
+		if(user.id === id) {
+			found =true;
+			return res.json(user);
+		} 
+	})
+	if (!found) {
+		res.status(400).json('not found');
+	}
+})
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user =>{
+		if(user.id === id) {
+			found = true;
+			user.entries++
+			return res.json(user.entries);
+		} 
+	})
+	if (!found) {
+		res.status(400).json('not found');
+	}
+
 })
 
 app.listen(3000, () => {
